307ea85162e2e2e3c941731a260845683d215861
yeseul106 <20191037@sungshin.ac.kr>
2022-11-23 01:58:26 +0900

    [FIX] 빠른 예매 기능 수정 완료

--- src/controller/ticketsController.ts
+++ src/controller/ticketsController.ts
@@ -4,9 +4,9 @@ import { ticketsService } from "../service";
 const createTicket = async (req: Request, res: Response) => {
     
     const { userId , title, cinemaRegion, cinemaState, date, time, amount } = req.body; //비구조 할당
+    // DTO로 바꾸자
 
-
-    // 유효하지 않은 데이터 검사 어케 하지 ?
+    // 유효하지 않은 데이터 검사 어케 하지 ? (service에서 모듈로 검사 로직 수행하고 결과값을 controller에서 받아서 예외처리)
 
     // 데이터가 다 넘어오지 않았을 떄
     if (!userId || !title || !cinemaRegion || !cinemaState || !date || !time || !amount) { 
@@ -14,7 +14,7 @@ const createTicket = async (req: Request, res: Response) => {
       }
     
     // 디비 통신하도록 넘겨주기 (controller의 역할)
-    const data = await ticketsService.createTicket(userId , title, cinemaRegion, cinemaState, date, time, amount);
+    const data = await ticketsService.createTicket(userId , title, cinemaRegion, cinemaState, date, time, +amount);
         
     if (!data) {
         return res.status(400).json({ status: 400, message: "유저 생성 실패" });
--- src/service/ticketsService.ts
+++ src/service/ticketsService.ts
@@ -1,10 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const createTicket = async (userId: string , titleParam: string , cinemaRegion: string , cinemaState: string, 
-    date: Date, time: string, amount: number) => {
+/**
+ * @api {post} /tickets 빠른 예매 하기
+ *
+ *
+ * @apiSuccess {status} success API 상태 코드
+ * @apiSuccess {Boolean} success API 호출 성공 여부
+ * @apiSuccess {String} message 응답 메시지
+ * @apiSuccess {Object} 유저 아이디 & 예매 번호
+ */
+const createTicket = async (userId: string , title: string , cinemaRegion: string , cinemaState: string, 
+    date: string, time: string, amount: number) => {
 
-  const data = await prisma.ticketing.create({
+  const movie = await prisma.movie.findUnique({
+  where : {
+    movieTitle : title
+  },
+ })
+
+  /**
+  * @desc 상영 정보 조회
+  */
+ const screening = await prisma.screening.findFirst({
+  where : {
+    time: time,
+    date: date,
+    movieId : movie?.id
+  }
+ })
+
+  const ticket = await prisma.ticketing.create({
     data: {
       reservationDate: date,
       amount,
@@ -15,13 +41,13 @@ const createTicket = async (userId: string , titleParam: string , cinemaRegion: 
       },
       screening : {
         connect : {
-          id: 1 // 임의로 id 지정 -> 중첩으로 connect ?..
+          id: screening?.id
         },
         }
       }
     });
 
-  return data;
+  return ticket;
 }; 
 
 const ticketsService = {
