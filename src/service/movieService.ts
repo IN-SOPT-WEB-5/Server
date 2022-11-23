5c73bb0405b58c9e283e101c838bdf6c8e1a1c45
Hayan Jo <59385635+yanh2@users.noreply.github.com>
2022-11-23 17:01:56 +0900

    [FIX] 박스오피스 조회 기능 수정

--- src/service/movieService.ts
+++ src/service/movieService.ts
@@ -2,13 +2,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 //* 박스오피스 전체 조회
-const getAllMovie = async() => {
-    const data = await prisma.movie.findMany();
-    return data;
+const getAllMovie = async () => {
+  const data = await prisma.movie.findMany({
+    orderBy: {
+      advanceRate: "desc", //예매율 기준 내림차순 정렬하여 출력
+    },
+  });
+  return data;
 };
 
 const movieService = {
-    getAllMovie,
+  getAllMovie,
 };
-  
+
 export default movieService;
