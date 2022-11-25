b0227e5034238c302f7c251521db555f64c5de14
Hayan Jo <59385635+yanh2@users.noreply.github.com>
2022-11-25 15:39:30 +0900

    [FIX] CORS error, 무한로딩 해결

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
 
+var cors = require("cors");
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
 
+app.use(cors());
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
+/*
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true'); });
-  
-  app.use("/", router);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+});
+*/
+
+app.use("/", router);
 
 //* HTTP method - GET
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
